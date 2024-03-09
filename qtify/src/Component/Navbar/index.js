d205ccf541ab4eb520f1f35a10bdfdb2dec3f540
Aditya Gupta <adityaguptacse@gmail.com>
2024-03-08 22:37:57 -0800

    Navbar

--- qtify/src/Component/Navbar/index.js
+++ qtify/src/Component/Navbar/index.js
@@ -8,7 +8,7 @@ export default function Navbar(){
         <div className="navbar">
             {/* <img src='logo.png' width={67} height={37} alt='logo'/> */}
             <Logo />
-            <SearchBox placeholder="search an item of your choice"/>
+            <SearchBox placeholder="search"/>
             <Button>Give Feedback</Button>
 
         </div>
